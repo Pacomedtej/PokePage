db82ddc4be15aaf706a26ad0d343eab922581a02
fcom <pacomedtej@gmail.com>
2024-11-15 20:33:35 -0600

    Modificaciones a los estilos y Nuevos Pokémon

--- vue/app.js
+++ vue/app.js
@@ -3,20 +3,6 @@ let appVue = new Vue({
     data:{
         trainerName: '',
         selectedPokemon: 'Pikachu',
-        pokemonImages: {
-            "Pikachu": {
-                "url": "https://i.gifer.com/4bXB.gif"
-            },
-            "Squirtle": {
-                "url": "https://i.gifer.com/DD0.gif"
-            },
-            "Oshawott": {
-                "url": "https://i.gifer.com/23xi.gif"
-            },
-            "Charmander": {
-                "url": "https://pa1.aminoapps.com/6777/acc878aee328657849329f25d55198da55da639f_hq.gif"
-            }
-        }
     },
     methods:{
 
